db30d9b63613d00a88a156d7e4f09a2ed4cd2545
RealYarik89 <yarok.89@gmail.com>
2024-03-14 02:23:38 +0300

    fixed bag

--- app.js
+++ app.js
@@ -8,28 +8,29 @@ button.addEventListener("click", () => {
         let latitude = position.coords.latitude
         let long = position.coords.longitude
 
-        let res = await fetch(`api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${long}&appid=${apiToken}&lang=ru`)
+        let res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${long}&appid=${apiToken}&lang=ru`)
         let resJson = await res.json()
-
+        console.log(resJson)
         let city = document.querySelector(".city")
         city.innerText = resJson.name
 
         let temp = document.querySelector(".temp")
-        temp.innerText = `${resJson.main.temp - 272} &#176 C`
+        temp.innerText = `${Math.round(resJson.main.temp - 272)}° C`
 
         let icon = document.querySelector(".icon")
         let img = document.createElement("img")
-        img.src = `https://openweathermap.org/img/wn/${resJson.weather.icon}@2x.png`
+
+        img.src = `https://openweathermap.org/img/wn/${resJson.weather[0].icon}@2x.png`
         icon.appendChild(img)
 
-        let discription = document.querySelector("discription")
-        discription.innerText = resJson.weather.description
+        let discription = document.querySelector(".discription")
+        discription.innerText = resJson.weather[0].description
 
-        let speed = document.querySelector("speed")
-        speed.innerText = `${resJson.wind.speed} м/с`
+        let speed = document.querySelector(".speed")
+        speed.innerText = `Ветер: ${resJson.wind.speed} м/с`
 
-        let humidity = document.querySelector("humidity")
-        humidity.innerText = `${resJson.main.humidity}%`
+        let humidity = document.querySelector(".humidity")
+        humidity.innerText = `Влажность: ${resJson.main.humidity}%`
     }
 
     navigator.geolocation.getCurrentPosition(succesGetLocation)
